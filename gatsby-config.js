99b50f5862ddffc73dfeef4e0a7cf0638117312f
noe nevarez <closner_nevarez@hotmail.com>
2019-10-22 13:34:16 +0700

    updated title and description for site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `My First Gatbsy Build Blog`,
+    description: `My first gatsby built blog post..`,
     author: `@gatsbyjs`,
   },
   plugins: [
